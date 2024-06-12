f5a3227ba6659985644894869616f5a503df7614
SNEHAIP <snehaprasheed12@gmail.com>
2024-06-13 00:00:25 +0530

    navbar

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import NavBar from './NavBar'
+import axios from 'axios'
 
 const ViewAll = () => {
+    const [data,changeData]=useState([])
+    const fetchData =()=>{
+       axios.post("http://localhost:8080/view") .then(
+        (response)=>{
+            changeData(response.data)
+        }
+       ).catch().finally()
+    }
+    useEffect(()=>{fetchData()},[])
     return (
         <div>
+            <NavBar/>
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -11,19 +23,27 @@ const ViewAll = () => {
                                 <tr>
                                     <th scope="col">DoctorId</th>
                                     <th scope="col">DoctorName</th>
-                                    <th scope="col">Specialisation</th>
+                                    <th scope="col">Specialization</th>
                                     <th scope="col">Address</th>
                                     <th scope="col">MobileNo</th>
                                     <th scope="col">Email</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
+                              {data.map(
+                                (value,index)=>{
+                                    return    <tr>
+                                    <th scope="row">{value.doctorid}</th>
+                                    <td>{value.doctorname}</td>
+                                    <td>{value.specialization}</td>
+                                    <td>{value.address}</td>
+                                    <td>{value.mobno}</td>
+                                    <td>{value.address}</td>
                                 </tr>
+                                }
+                              )
+
+                              }
                                
                             </tbody>
                         </table>
